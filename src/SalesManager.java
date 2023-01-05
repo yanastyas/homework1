cbb7d4e58c6c9dbfb1ee3727d77ba5edf896a6a7
yanastyas <yanastyas28@mail.ru>
2023-01-06 02:39:17 +1000

    first commit

---
+++ src/SalesManager.java
@@ -0,0 +1,29 @@
+public class SalesManager {
+    protected int[] sales;
+
+    public SalesManager(int[] sales) {
+        this.sales = sales;
+    }
+
+    public int max() {
+        int max = -1;
+        for (int sale : sales) {
+            if (sale > max) {
+                max = sale;
+            }
+        }
+        return max;
+    }
+
+    public int min() {
+        int min = max();
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+}
+
